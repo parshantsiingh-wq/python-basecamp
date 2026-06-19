0f2783d30c483571ddf8c4b1df5561129a3302d2
Parshant Singh <parshantsiingh@gmail.com>
2026-06-19 23:20:56 +0530

    Add files via upload

---
+++ 14_infinite_loop_controls.py
@@ -0,0 +1,18 @@
+i=0
+while(True):
+    print(i,end=" ")
+    if i==30:
+        break # stop the loop
+    i=i+1
+
+j=0
+while(True):
+    if j+1<5:
+        j+=1
+        continue
+    print("value of j: ", j)
+    if j==44:
+        break
+    j=j+1
+
+
---
+++ 15_enumerate_function_basics.py
@@ -0,0 +1,3 @@
+l=[2,3,4,5,"parshant"]
+for indexs,value in enumerate(l):   # enumerate group with index and value:((0,2),(1,3),(2,4),(3,5),(3,"parshant"))
+    print(f"indexs_{indexs} value is: {value}")
