bec4e719ffc174799dee93c09a3ed4f7a0a14cee
Parshant Singh <parshantsiingh@gmail.com>
2026-06-25 19:29:02 +0530

    Add files via upload

---
+++ 03_Logic_Building/17_mini_word_dictionary.py
@@ -0,0 +1,5 @@
+dict={"mutable":"change", "im-mutable": "cant-change", "internship": "practical_work", "worship": "god"}
+print(dict)
+print("enter the string:\n")
+string = input()
+print("your string meaning is :", dict[string])
